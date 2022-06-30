59a32c8a8130dc6f3278ca9b4a4b50f677b92527
Nil Borodulia <nil.b@oxtech.org>
2022-07-01 00:49:56 +0100

    add swoole hendlers

--- src/SwooleWebSocket/WebSocket.php
+++ src/SwooleWebSocket/WebSocket.php
@@ -4,29 +4,53 @@ namespace Jtrw\Micro\Poc\Rpc\SwooleWebSocket;
 
 use Swoole\WebSocket\Server;
 use Swoole\WebSocket\Frame;
+use Swoole\Http\Request;
 
 class WebSocket
 {
-    protected Server $webSocket;
+    protected Server $server;
     
-    public function __construct()
+    public function __construct(array $options)
     {
-        $this->webSocket = new Server('0.0.0.0', 9502);
+        $host = $options['host'] ?? '';
+        $port = $options['port'] ?? '';
+        $this->server = new Server($host, $port);
     }
     public function start()
     {
-        $this->webSocket->on(
+        $this->server->on("Start", function(Server $server)
+        {
+            echo "Swoole WebSocket Server started at 127.0.0.1:9501\n";
+        });
+    
+        $this->server->on('Open', function(Server $server, Request $request)
+        {
+            echo "connection open: {$request->fd}\n";
+        
+            $server->tick(1000, function() use ($server, $request)
+            {
+                $server->push($request->fd, json_encode(["hello", time()]));
+            });
+        });
+        
+        $this->server->on(
             'message',
             function (Server $ws, Frame $frame) {
                 $data = json_decode($frame->data, true, JSON_THROW_ON_ERROR);
 
                 $instance = $this->getControllerInstance($data);
-                $responseDto = $instance->apply($data['data']);
+                $responseDto = $instance->apply($data['params']);
                 
                 $ws->push($frame->fd, $responseDto->toJson());
             }
         );
-        $this->webSocket->start();
+    
+        $this->server->on('Close', function(Server $server, int $fd)
+        {
+            echo "connection close: {$fd}\n";
+        });
+        
+        $this->server->start();
     }
     
     protected function getControllerInstance(array $data): WebsocketJsonRpcMethodInterface
